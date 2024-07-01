a4c0202f2c039ac2d88b99d850d6f33cf1e3853f
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-01 04:33:57 +0000

    add files to repository

---
+++ springboot-log4j-api/src/test/java/com/example/springboot_log4j_api/SpringbootLog4jApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_log4j_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLog4jApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
